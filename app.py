b424fe5f29e393e15b8f1a44ac3f292e5247462b
amiicao <85824514+amiicao@users.noreply.github.com>
2024-10-23 00:57:18 -0700

    Update app.py
    set logger to `logging.DEBUG` for developmental purposes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from src.webhook_handler import webhook_blueprint
 app = Flask(__name__)
 app.register_blueprint(webhook_blueprint)
 logging.basicConfig()
-logging.getLogger().setLevel(logging.INFO)
+logging.getLogger().setLevel(logging.DEBUG)
 
 if __name__ == "__main__":
     app.run(port=4000)
